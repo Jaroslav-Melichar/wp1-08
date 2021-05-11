6b1a5aba6f556b43b00bbffda47f4574b80111d6
Jaroslav-Melichar <jarabaltazar@seznam.cz>
2021-05-11 11:40:04 +0200

    Update index.php

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oblíbené kategorie her</title>
+</head>
+<body>
+<h1>Vyber oblíbené herní žánry</h1>
+</br>
+<input type="checkbox" id="game" name="genres[]">
+<label for="game">Horrorovky</label><br>
+</br>
+<input type="checkbox" id="game" name="genres[]">
+<label for="game">Akční</label><br>
+</br>
+<input type="checkbox" id="game" name="genres[]">
+<label for="game">Adventury</label><br>
+</br>
+<input type="checkbox" id="game" name="genres[]">
+<label for="game">Hentai</label><br>
+</br>
+<input type="checkbox" id="game" name="genres[]">
+<label for="game">Střílečky</label><br>
+</br>
+<input type="checkbox" id="game" name="genres[]">
+<label for="game">Závodní</label><br>
+</br>
+<input type="checkbox" id="game" name="genres[]">
+<label for="game">Sportovní</label><br>
+</br>
+<input type="checkbox" id="game" name="genres[]">
+<label for="game">Animované</label><br>
+</br>
+<input type="checkbox" id="game" name="genres[]">
+<label for="game">Realistické</label> </br>
+<br>
+<input type="checkbox" id="game" name="genres[]">
+<label for="game">Plošinové</label> </br>
+
+    
+</body>
+</html>
