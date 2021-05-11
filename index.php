c8d1fcb6776d4a3cca8c5798ce1cf86b7f015c16
Jaroslav-Melichar <jarabaltazar@seznam.cz>
2021-05-11 12:22:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-$submit = filter_input(INPUT_POST, 'submit' )
+$submit = filter_input(INPUT_POST, 'submit' );
+$genres = $_POST['genres00'];
+var_dump($genres);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,34 +24,34 @@ echo"Formulář byl odeslán";
 
 <h1>Vyber oblíbené herní žánry</h1>
 </br>
-<input type="checkbox" id="game" name="genres[]">
+<input type="checkbox" id="genre1" name="genres[]" value="Horror">
 <label for="game">Horrorovky</label><br>
 </br>
-<input type="checkbox" id="game" name="genres[]">
+<input type="checkbox" id="genre2" name="genres[]" value="Action">
 <label for="game">Akční</label><br>
 </br>
-<input type="checkbox" id="game" name="genres[]">
+<input type="checkbox" id="genre3" name="genres[]" value="Adventure">
 <label for="game">Adventury</label><br>
 </br>
-<input type="checkbox" id="game" name="genres[]">
+<input type="checkbox" id="genre4" name="genres[]" value="Hentai">
 <label for="game">Hentai</label><br>
 </br>
-<input type="checkbox" id="game" name="genres[]">
+<input type="checkbox" id="genre5" name="genres[]" value="FPS">
 <label for="game">Střílečky</label><br>
 </br>
-<input type="checkbox" id="game" name="genres[]">
+<input type="checkbox" id="genre6" name="genres[]" value="Race">
 <label for="game">Závodní</label><br>
 </br>
-<input type="checkbox" id="game" name="genres[]">
+<input type="checkbox" id="genre7" name="genres[]" value="Sport">
 <label for="game">Sportovní</label><br>
 </br>
-<input type="checkbox" id="game" name="genres[]">
+<input type="checkbox" id="genre8" name="genres[]" value="Anime">
 <label for="game">Animované</label><br>
 </br>
-<input type="checkbox" id="game" name="genres[]">
+<input type="checkbox" id="genre9" name="genres[]" value="Realistic" >
 <label for="game">Realistické</label> </br>
 <br>
-<input type="checkbox" id="game" name="genres[]">
+<input type="checkbox" id="genre10" name="genres[]" value="Place" >
 <label for="game">Plošinové</label> </br>
 <br>
 </br>
